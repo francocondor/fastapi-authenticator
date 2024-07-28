a73bfaa923fdd346bdd0859bc908b411d172d52a
francobko <franco.condor@urp.edu.pe>
2024-07-27 20:01:53 -0500

    feat: Add username and password parameters to login endpoint

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.post("/token")
-def login():
+def login(username = Form(), password = Form()):
     return "token"
