d52bf4eaa72c377aefd636a999a952fdfd88d97c
francobko <franco.condor@urp.edu.pe>
2024-07-27 19:57:52 -0500

    chore: Add OAuth2 authentication for token endpoint

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.get("/")
-def home():
-    return "Hi"
+@app.post("/token")
+def login():
+    return "token"
