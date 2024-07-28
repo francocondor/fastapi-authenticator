72e1c9be88e49f8f7ea18e666799e3e566bb3d61
francobko <franco.condor@urp.edu.pe>
2024-07-27 20:24:13 -0500

    feat: Add token authentication for profile endpoint

--- main.py
+++ main.py
@@ -19,9 +19,20 @@ users = {
     },
 }
 
+def encode_token(payload: dict)-> str:
+    return ""
+
+def decode_token(token: str)-> dict:
+    return {}
+
 @app.post("/token")
 def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = users.get(form_data.username)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username")
-    return user
+    token = encode_token({"username": user["username"], "email": user["email"]})
+    return {"access_token": token}
+
+@app.get("/users/profile")
+def profile(my_user: Annotated[dict, Depends(decode_token)]):
+    return my_user
