1e99f3b8d320023dccf46a614c01b39924ca8c5b
francobko <franco.condor@urp.edu.pe>
2024-07-27 20:14:43 -0500

    chore: Refactor login endpoint to use OAuth2PasswordRequestForm data class

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Form
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.post("/token")
-def login(username = Form(), password = Form()):
-    return "token"
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    return form_data
